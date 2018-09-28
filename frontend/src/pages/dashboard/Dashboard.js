40e940262e544f4f42a8cd6f3f269eae728ca9f7
Dan Florio <dflo@danflorio.com>
2018-09-28 11:15:17 -0400

    change dashboard message

--- frontend/src/pages/dashboard/Dashboard.js
+++ frontend/src/pages/dashboard/Dashboard.js
@@ -10,8 +10,7 @@ const Dashboard = (props) => (
   <div className="dashboard">
     <h1 className="dashboard__header">Hello, {props.username}!</h1>
     <p className="dashboard__tagline">
-      Why not head over to <a className="dashboard__link" href="https://www.shakacode.com/">ShakaCode</a> and
-      explore the forums?
+      Do you feel cool now?
     </p>
   </div>
 );
